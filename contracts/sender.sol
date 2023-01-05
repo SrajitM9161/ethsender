1edd47d6364d685334153a1f893682a64b9338b9
Srajit Mishra <106705809+SrajitM9161@users.noreply.github.com>
2023-01-05 15:16:13 +0530

    Update and rename .sol to sender.sol

--- contracts/sender.sol
+++ contracts/sender.sol
@@ -1,28 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
-contract chai {
-    struct Memo {
+contract harsh {
+    struct m1 {
         string name;
         string message;
         uint256 timestamp;
         address from;
     }
 
-    Memo[] memos;
+    m1[] m;
     address payable owner;
 
     constructor() {
         owner = payable(msg.sender);
     }
 
-    function buyChai(string memory name, string memory message) public payable {
+    function sendingmoney(string memory name, string memory message) public payable {
         require(msg.value > 0, "Please pay greater than 0 ether");
         owner.transfer(msg.value);
-        memos.push(Memo(name, message, block.timestamp, msg.sender));
+        memos.push(m1(name, message, block.timestamp, msg.sender));
     }
 
-    function getMemos() public view returns (Memo[] memory) {
-        return memos;
+    function getm1() public view returns (m1[] m) {
+        return m
+        ;
     }
 }
